1bcf43d76805dece1ce7965d3a878b411b0c07fd
Matheus Waked Franca <matheus363@hotmail.com>
2022-06-03 15:51:57 -0300

    Inital commit for kotlin calculator.

---
+++ Calculator/src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+fun main(args: Array<String>) {
+    println("Welcome to my Kotlin calculator.")
+    println("For using it just input two numbers and then choose an operation.")
+
+    var number1:Int?
+    var number2:Int?
+    var operation:Int
+
+    println("What is the first number?")
+    number1 = readLine()?.toInt() ?: 0
+
+    println("What is the second number?")
+    number2 = readLine()?.toInt() ?: 0
+
+    println("Now what is the operation?")
+    println("0 - summ, 1 - subtraction, 2 - multiplication, 3 - division")
+    operation = readln().toInt()
+
+    calculate(number1, number2, operation)
+}
+
+fun calculate(a:Int, b:Int, op:Int){
+
+
+    when(op){
+        0 -> println(a + b)
+        1 -> println(a - b)
+        2 -> println(a * b)
+        3 -> println(a / b)
+        else -> println("Wrong operation chosen.")
+    }
+}
